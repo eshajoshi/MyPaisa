42307d9e99b4b5770e144d9bded1bf21f13a2bb3
Esha Joshi <ejoshi@apple.com>
2017-11-15 18:06:45 -0800

    Initial Xcode files

---
+++ MyPaisa/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyPaisa
+//
+//  Created by Esha Joshi on 11/15/17.
+//  Copyright © 2017 Esha Joshi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
