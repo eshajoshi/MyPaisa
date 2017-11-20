a15f37a5d6aa2907144afdc8bf825b5f9e4c8926
Esha Joshi <ejoshi@apple.com>
2017-11-20 12:52:32 -0800

    Added MyPaisaKit to project
    Dragged MyPaisaKit.framework from the ‘Products’ folder of .xcodeproj into the ‘Embedded Binaries’ and ‘LinkedFramework and Binaries’ sections

---
+++ MyPaisaKit/MyPaisaKit/MyPaisaKit.h
@@ -0,0 +1,19 @@
+//
+//  MyPaisaKit.h
+//  MyPaisaKit
+//
+//  Created by Esha Joshi on 11/20/17.
+//  Copyright © 2017 Esha Joshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyPaisaKit.
+FOUNDATION_EXPORT double MyPaisaKitVersionNumber;
+
+//! Project version string for MyPaisaKit.
+FOUNDATION_EXPORT const unsigned char MyPaisaKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyPaisaKit/PublicHeader.h>
+
+
